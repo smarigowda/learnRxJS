c6815f12ea80c23910b92074de99844151da20b1
Santosh <santosharakere@gmail.com>
2015-10-06 21:38:43 +0100

    removing exercies

--- ex_001.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"],
-		counter;
-
-	for(counter = 0; counter < names.length; counter++) {
-		console.log(names[counter]);
-	}
-})();
